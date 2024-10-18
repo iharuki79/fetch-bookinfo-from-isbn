77436b1298d31552c2fde244f6f7e50644e1164f
iharuki79 <50833220+iharuki79@users.noreply.github.com>
2024-10-19 01:42:13 +0900

    Add sleep 0.1sec and Refactor code to improve readability

--- read_ISBN.py
+++ read_ISBN.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from PIL import Image
 from pyzbar.pyzbar import decode, ZBarSymbol
 import requests
@@ -16,6 +17,14 @@ ns = {
 
 
 def read_ISBN_from_image(img_path):
+    """画像パスからISBNコードを読み取り、書籍情報を取得したリストを返す
+
+    Args:
+        img_path (str): 画像パス
+
+    Returns:
+        list[str]|None: 取得した書籍名のリスト
+    """
     try:
         img = Image.open(img_path)
     except FileNotFoundError:
@@ -34,6 +43,14 @@ def read_ISBN_from_image(img_path):
 
 
 def read_ISBN(decoded_data):
+    """pyzbar.decode()で読み取ったデータからISBNコードだけを取得し、書籍情報を取得する
+
+    Args:
+        decoded_data (Decoded): pyzbar.decode()で読み取ったデータ
+
+    Returns:
+        dict[str,str]|None: タイトルと著者のリスト
+    """
     if decoded_data.type != "EAN13" and decoded_data.type != "EAN10":
         return None
     if decoded_data.data.decode("utf-8")[:2] != "97":
@@ -43,8 +60,16 @@ def read_ISBN(decoded_data):
     return get_book_info(book_info)
 
 
-# 国立国会図書館のAPIを使って書籍情報を取得する
 def fetch_info_from_ndl(isbn_code, output_file=None):
+    """国会図書館のAPIを使って書籍情報を取得する
+
+    Args:
+        isbn_code (str): ISBNコード(13桁か10桁で先頭が"97"であること)
+        output_file (str, optional): 出力先のファイル. Defaults to None.
+
+    Returns:
+        str: 書籍情報のXMLテキスト
+    """
     assert len(isbn_code) == 13 or len(isbn_code) == 10
     url = (
         "https://iss.ndl.go.jp/api/sru?"
@@ -58,6 +83,7 @@ def fetch_info_from_ndl(isbn_code, output_file=None):
         + '" AND dpid=iss-ndl-opac'
     )
     response = requests.get(url)
+    time.sleep(0.1)  # 連続アクセスを避けるために0.1秒待つ
     if output_file is not None:
         with open(f"{output_file}.xml", "w", encoding="utf-8") as f:
             f.write(response.text)
@@ -65,6 +91,14 @@ def fetch_info_from_ndl(isbn_code, output_file=None):
 
 
 def get_book_info(book_info):
+    """国会図書館のAPIから取得した書籍情報XMLからタイトルと著者を取得する
+
+    Args:
+        book_info (str): APIで得られた書籍情報のXMLテキスト
+
+    Returns:
+        dict[str,str]|None: title, authorをキーとしたタイトルと著者の辞書
+    """
     if book_info is None:
         return None
     root = ET.fromstring(book_info)
